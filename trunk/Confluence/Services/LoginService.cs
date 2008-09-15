39cb941cce709c57daaea982d5416cc91e0dd704
fernandezpablo85 <fernandezpablo85@7e2e938f-3a55-0410-ad99-25f52660d43b>
2008-09-15 15:52:08 +0000

    Added Spring.net DI.

---
+++ trunk/Confluence/Services/LoginService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Confluence.Services
+{
+    public class LoginService : ILoginService
+    {
+        public LoginService() { }
+
+        public bool doLogin(string userName, string password)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+    }
+}
