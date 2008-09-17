0508db9901d9e0a272f678b25af20083c7f71788
fernandezpablo85 <fernandezpablo85@7e2e938f-3a55-0410-ad99-25f52660d43b>
2008-09-17 16:03:21 +0000

    Minor refactor to Authz System.

--- trunk/Confluence/Web/App_Code/Helpers/SecuritySpec.cs
+++ trunk/Confluence/Web/App_Code/Helpers/SecuritySpec.cs
@@ -12,14 +12,19 @@ using System.Collections.Generic;
 
 public static class SecuritySpec
 {
-    public static bool validateUser(User user,int patente)
+    public static bool canAccessPage(User user,int patente)
     {
-        if (user == null) return false;
+        if (!isLoggedIn(user)) return false;
 
         bool hasIt = false;
         foreach (Patente pat in user.Patentes)
             if (pat.Id == patente) hasIt = true;
 
         return hasIt;
+    }
+
+    public static bool isLoggedIn(User user)
+    {
+        return (user != null);
     }
 }
