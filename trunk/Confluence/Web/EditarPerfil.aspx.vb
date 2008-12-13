1de9f93203689894325796643203f9a696f551df
fernandezpablo85 <fernandezpablo85@7e2e938f-3a55-0410-ad99-25f52660d43b>
2008-12-13 19:15:28 +0000

    [commit para evitar conflictos] fixed User Profile

--- trunk/Confluence/Web/EditarPerfil.aspx.vb
+++ trunk/Confluence/Web/EditarPerfil.aspx.vb
@@ -1,4 +1,5 @@
 Imports Confluence.Services
+Imports Confluence.Domain
 
 Partial Class EditarPerfil
     Inherits PrivatePage
@@ -18,10 +19,22 @@ Partial Class EditarPerfil
     End Sub
 
     Public Overrides Sub On_Load(ByVal sender As Object, ByVal e As System.EventArgs)
-        If (RegistryService.IsHR(ActiveUser.Name)) Then
-            type.Text = "Recurso Humano"
-        Else
-            type.Text = "Demandante"
-        End If
+        If Page.IsPostBack Then Return
+        Dim activeclient As Client = RegistryService.GetClientFromAccountName(ActiveUser.Name)
+        name.Text = activeclient.Name
+        mail.Text = ActiveUser.Mail
+        country.Text = activeclient.Country
+        state.Text = activeclient.State
+        phone.Text = activeclient.Phone.ToString()
+
+    End Sub
+
+    Protected Sub ModificarPerfil(ByVal sender As Object, ByVal e As System.EventArgs) Handles modificar.Click
+        RegistryService.UpdateProfile(ActiveUser.Name, name.Text, mail.Text, country.Text, state.Text, phone.Text)
+        Response.Redirect(Constants.Redirects.USER_PROFILE)
+    End Sub
+
+    Protected Sub Cancelar_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cancelar.Click
+        Response.Redirect(Constants.Redirects.USER_PROFILE)
     End Sub
 End Class
